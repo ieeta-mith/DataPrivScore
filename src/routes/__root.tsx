5e8de3146385b2adcd09b3fcda7548878a79bd08
zegameiro <jose.mcgameiro@ua.pt>
2026-02-04 15:58:40 +0000

    Create root layout with navbar and footer

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,15 @@
-import * as React from 'react'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { Navbar } from '@/components/navbar'
+import { Footer } from '@/components/footer'
 
-export const Route = createRootRoute({
-  component: RootComponent,
-})
-
-function RootComponent() {
+const RootLayout = () => {
   return (
-    <React.Fragment>
-      <div>Hello "__root"!</div>
+    <>
+      <Navbar />
       <Outlet />
-    </React.Fragment>
+      <Footer />
+    </>
   )
 }
+
+export const Route = createRootRoute({ component: RootLayout })
